4cfdc07575562d9eb4e75605f62b848985002696
xavierdelacruz <xdelacruz89@gmail.com>
2020-06-28 15:47:42 -0700

    Added remove Nth Node from End of list

---
+++ 19 - Remove Nth node From End Of List.cs
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        
+        if (head == null) {
+            return null;
+        }
+        
+        if (n <= 0) {
+            return head;
+        }
+        // Find length of the entire linked list first.
+        // If odd, where fastPtr.next is null, then we do ((slowptr index + 1) * 2) - 1
+        // If even, where fastPtr is null, then we do (slowptr index/2 + 1) * 2
+        var length = FindLength(head);
+        var targetIndex = length - n;
+        
+        if (targetIndex <= 0) {
+            return head.next;
+        }
+    
+        var headRef = head;
+        ListNode prev = null;
+        var index = 0;
+        
+        while (index <= targetIndex) {
+            if (index == targetIndex) {
+                prev.next = headRef.next;
+                headRef = null;
+                break;
+            }
+            index++;
+            prev = headRef;
+            headRef = headRef.next;
+            
+        }
+        return head;      
+    }
+    
+    // Helper function, at most O(n), since we only need to traverse half the list
+    // using the fast pointer technique when finding the midpoint.
+    private int FindLength(ListNode head) {
+        var slowPtr = head;     
+        var fastPtr = head;
+        
+        int slowNodeVisited = 0;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            slowNodeVisited++;
+        }
+        
+        if (fastPtr == null) {
+            return slowNodeVisited * 2;
+        } else {
+            return ((slowNodeVisited + 1) * 2) - 1;
+        }
+    }
+}
