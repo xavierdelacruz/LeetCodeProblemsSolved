d6fca9edbde048b517f0d5d735dfcc1c16c22887
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-01 19:52:18 -0700

    Added some changes

--- 287 - Find the Duplicate Number.cs
+++ 287 - Find the Duplicate Number.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
+        
+        // Not necessary, but I like just to have it here.
+        if (nums.Length == 0 || nums.Length == 1) {
+            return -1;
+        }
+        
         var tortoise = nums[0];
         var hare = nums[0];
         
@@ -7,13 +13,15 @@ public class Solution {
         tortoise = nums[tortoise];
         hare = nums[nums[hare]];
         
+        // Find intersection point of the tortoise and hare
         while (hare != tortoise) {
             tortoise = nums[tortoise];
             hare = nums[nums[hare]];
         }
         
-        tortoise = nums[0];
-        
+        // Now let the tortoise start from the beginning again.
+        // When they meet again, the current value of the hare will be the duplicate.
+        tortoise = nums[0];     
         while (hare != tortoise) {
             hare = nums[hare];
             tortoise = nums[tortoise];
