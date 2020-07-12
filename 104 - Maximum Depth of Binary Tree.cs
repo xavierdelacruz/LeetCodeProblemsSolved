1d790fe07149715a1618b497bad8e0d21b0a7ba0
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-12 11:16:46 -0700

    Added iterative tree max depth

---
+++ 104 - Maximum Depth of Binary Tree.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {         
+        if (root == null) {
+            return 0;
+        }
+        
+        var q = new Queue<TreeNode>();
+        q.Enqueue(root);
+        
+        var level = 0;
+        
+        // O(n) operation - BFS algorithm
+        while (q.Count != 0) {          
+            var qCount = q.Count;            
+            level++;           
+            while (qCount > 0) {
+                var curr = q.Dequeue();
+                qCount--;
+                if (curr.right != null) {
+                    q.Enqueue(curr.right);
+                }             
+                if (curr.left != null) {
+                    q.Enqueue(curr.left);
+                }
+            }
+        }        
+        return level;
+    }   
+}
