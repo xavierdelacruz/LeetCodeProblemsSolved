d1180a60a2690d1987bdecb11e2017f150b4e3c7
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-12 12:28:03 -0700

    Added level order traversal reverse

---
+++ 107 - Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrderBottom(TreeNode root) {
+        
+        var res = new List<IList<int>>();
+        var currList = new List<int>();
+        
+        if (root == null) {
+            return res;
+        }
+        
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        
+        while (queue.Count != 0) {           
+            var qCount = queue.Count;       
+            while (qCount > 0) {
+                var curr = queue.Dequeue();
+                currList.Add(curr.val);
+                qCount--;
+                
+                if (curr.left != null) {
+                    queue.Enqueue(curr.left);
+                }
+                
+                if (curr.right != null) {
+                    queue.Enqueue(curr.right);
+                }  
+            }         
+            res.Add(currList);
+            currList = new List<int>();
+        }        
+        // Reversal takes O(n)
+        res.Reverse();
+        return res;        
+    }
+}
