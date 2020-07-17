196d37ff59b588e421bb8a1490b8a315c88a32ff
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-16 17:39:18 -0700

    Added recursive DFS

--- 200 - Number of Islands.cs
+++ 200 - Number of Islands.cs
@@ -6,15 +6,37 @@ public class Solution {
         
         var startPoints = GetStartPoints(grid);    
         // Keeps track of visited nodes.
+        var claimed = new HashSet<(int x, int y)>();
         var visited = new HashSet<(int x, int y)>();
         var islands = 0;
         foreach (var start in startPoints) {
+            
+            // For DFS, change this to claimed
             if (!visited.Contains(start)) {
-                BFS(grid, visited, start.x, start.y);
+                //var visited = new HashSet<(int x, int y)>();
+                DFS(grid, visited, claimed, start.x, start.y);
+                //BFS(grid, visited, start.x, start.y);
                 islands++;
             }         
         }   
         return islands;
+    }
+    
+    private void DFS(char[][] grid, HashSet<(int x, int y)> visited, HashSet<(int x, int y)> claimed, int x, int y) {
+        var dirs = new List<(int x, int y)>() { (1, 0), (-1, 0), (0, 1), (0, -1) };
+        
+        visited.Add((x, y));
+        
+        foreach (var dir in dirs) {
+            var newX = x + dir.x;
+            var newY = y + dir.y;
+                
+                // In BFS, set the node we are traversing as already-visited so we do not queue these nodes multiple times as we visit them.
+            if (IsTraverseable(grid, visited, newX, newY)) {
+                DFS(grid, visited, claimed, newX, newY);
+                claimed.Add((newX, newY));
+            }
+        }     
     }
     
     private void BFS(char[][] grid, HashSet<(int x, int y)> visited, int x, int y) {    
