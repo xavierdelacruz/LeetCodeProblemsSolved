b09776467b749ca720ffe0c2aad255ad8e8bd78f
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-21 22:12:16 -0700

    Changed problem 124

--- 124 - Binary Tree Maximum Path Sum.cs
+++ 124 - Binary Tree Maximum Path Sum.cs
@@ -14,44 +14,49 @@
 public class Solution {
     public int MaxPathSum(TreeNode root) {
         
+        // No paths, since there are no nodes.
         if (root == null) {
             return 0;
-        }        
-        var maxSum = Int32.MinValue;
-        FindSum(root, ref maxSum);            
-        return maxSum;
-    }
-    
-    // Using post order traversal.
-    // Calculate the rest on-the-go as we traverse the tree.
-    private int FindSum(TreeNode root, ref int sum) {
-        if (root == null) {
-            return 0;
-        }  
-        var left = FindSum(root.left, ref sum);
-        var right = FindSum(root.right, ref sum);
-        
-        // CASE 1: Both left and right are positive.
-        // Update the global sum to the total global result.
-        // Finally, return just one path with the larger subtree
-        if (left > 0 && right > 0) {      
-            // Check if the this global path result is the best so far that we've encountered.
-            sum = Math.Max(sum, left + right + root.val);
-            return Math.Max(left, right) + root.val;
-        } 
-        
-        // CASE 2: One or the other child is valid. Update global sum.
-        // Return the largest branch for further calculations.
-        if (left > 0 || right > 0) {
-            // Check if the this path result is the best so far that we've encountered.
-            sum = Math.Max(sum, Math.Max(left, right) + root.val);
-            return Math.Max(left, right) + root.val;
         }
         
-        // CASE 3: Neither branch are good. Compare the individual node with the best result so far.
-        // Just return root.val for future calculations.
-        // Finally, just update sum if root.val is the best so far that we've encountered.
-        sum = Math.Max(sum, root.val);
-        return root.val;     
+        // Only one path possible.
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        
+        // This handles the case if all nodes had negative values.
+        var max = Int32.MinValue;
+        
+        // There is no need to visit all nodes, since we bubble up with Post order.
+        // This means that we are generating results of smaller subtrees as we go up.
+        MaxPath(root, ref max);     
+        return max;
+        
+    }
+    
+    private int MaxPath(TreeNode root, ref int max) {
+        if (root == null) {
+            return 0;
+        }
+        
+        var left = MaxPath(root.left, ref max);
+        var right = MaxPath(root.right, ref max);
+        
+        if (left > 0 && right > 0) {
+            // Update max with the best possible left and right subtree paths, which pass through the current root.
+            // Then return the best branch from this so far.
+            max = Math.Max(max, root.val + left + right); 
+            return Math.Max(root.val + right, root.val + left);
+        } else if (left > 0 || right > 0) {     
+            // Update max with the best possible subranch that exists with root
+            // Then return the best branch so far
+            max = Math.Max(max, Math.Max(root.val + right, root.val + left));
+            return Math.Max(root.val + right, root.val + left);
+        } else {
+            // Otherwise, update max if the current root is better than what we've seen so far
+            // Then return the value of root.
+            max = Math.Max(root.val, max);            
+            return root.val; 
+        }           
     }
 }
