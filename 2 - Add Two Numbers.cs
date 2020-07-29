f7524c4fce0456f416a50ac044250585a89a38d3
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-29 12:19:07 -0700

    Improved and optimized solution

--- 2 - Add Two Numbers.cs
+++ 2 - Add Two Numbers.cs
@@ -8,65 +8,53 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-                
-        var head = l1;
-        var carry = 0;
-        var prev = l1;
         
-        if (l1 == null && l2 != null) {
-            Console.WriteLine(l2.val);
-            return l2;
-        } if (l1 != null && l2 == null) {
-            Console.WriteLine(l1.val);
-            return l1;
-        } else {
-            var remainder = 0;
-            while (l1 !=null || l2 != null || carry != 0) {                
-                if (l1 != null && l2 !=null) {
-                    var sum = l1.val + l2.val + carry;
-                    Console.WriteLine("Case 1 " + sum);
-                    carry = 0;
-                    remainder = sum % 10;
-                    if (sum >= 10) {                          
-                        carry = 1;
-                        Console.WriteLine("Carry is " + carry);
-                    }            
-                    l1.val = remainder;
-                    prev = l1;
-                    l1 = l1.next;
-                    l2 = l2.next;
-                } else if (l1 != null && l2 == null) {
-                    var sum = l1.val + 0 + carry;
-                    Console.WriteLine("Case 2 " + sum);
-                    carry = 0;
-                    remainder = sum % 10;
-                    if (sum >= 10) {                       
-                        carry = 1;
-                        Console.WriteLine("Carry is " + carry);
-                    }             
-                    l1.val = remainder;
-                    prev = l1;
-                    l1 = l1.next;
-                } else if (l1 == null && l2 != null) {
-                    var sum = 0 + l2.val + carry;
-                    Console.WriteLine("Case 3 " + sum);
-                    carry = 0;
-                    remainder = sum % 10;
-                    if (sum >= 10) {                        
-                        carry = 1;
-                        Console.WriteLine("Carry is " + carry);
-                    }
-                    l1 = new ListNode(remainder);
-                    prev.next = l1;
-                    prev = l1;
-                    l1 = l1.next;
-                    l2 = l2.next;
-                } else {
-                    prev.next = new ListNode(carry);
-                    break;
-                }
-            }         
+        if (l1 == null && l2 == null) {
+            return null;
         }
-        return head;            
+        
+        if (l1 == null) {
+            return l2;
+        }
+        
+        if (l2 == null) {
+            return l1;
+        }
+              
+        var carry = 0;
+        ListNode curr = new ListNode(0);
+        ListNode headRef = curr;
+        while (l1 != null || l2 != null) {
+            var curr1 = 0;            
+            if (l1 != null) {
+                curr1 = l1.val;
+            }
+            
+            var curr2 = 0;            
+            if (l2 != null) {
+                curr2 = l2.val;
+            }
+            
+            var sum = curr1 + curr2 + carry;
+            carry = sum/10;
+            var currDigit = sum%10;
+            curr.next = new ListNode(currDigit);
+            
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            
+            if (l2 != null) {
+                l2 = l2.next;  
+            }
+                
+        }
+        
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        
+        return headRef.next;
     }
 }
