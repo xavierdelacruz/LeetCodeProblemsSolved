ff21b53947adeba566baa3ecffe37a992bdde99f
xavierdelacruz <xdelacruz89@gmail.com>
2020-07-30 12:00:07 -0700

    Added recusive solution

--- 104 - Maximum Depth of Binary Tree.cs
+++ 104 - Maximum Depth of Binary Tree.cs
@@ -11,32 +11,51 @@
  *     }
  * }
  */
-public class Solution {
-    public int MaxDepth(TreeNode root) {         
-        if (root == null) {
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+        {
             return 0;
         }
-        
+
         var q = new Queue<TreeNode>();
         q.Enqueue(root);
-        
+
         var level = 0;
-        
+
         // O(n) operation - BFS algorithm
-        while (q.Count != 0) {          
-            var qCount = q.Count;            
-            level++;           
-            while (qCount > 0) {
+        while (q.Count != 0)
+        {
+            var qCount = q.Count;
+            level++;
+            while (qCount > 0)
+            {
                 var curr = q.Dequeue();
                 qCount--;
-                if (curr.right != null) {
+                if (curr.right != null)
+                {
                     q.Enqueue(curr.right);
-                }             
-                if (curr.left != null) {
+                }
+                if (curr.left != null)
+                {
                     q.Enqueue(curr.left);
                 }
             }
-        }        
+        }
         return level;
-    }   
+    }
+
+    // DFS recursive solution.
+    private int FindDepth(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var maxDepth = 1 + Math.Max(FindDepth(root.left), FindDepth(root.right));
+        return maxDepth;
+    }
 }
